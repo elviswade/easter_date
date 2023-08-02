6598b3e9bc5fa69858946148f5827d084e53a719
Augustus Buckman <b020112032@live.gtuc.edu.gh>
2023-08-02 17:47:35 +0000

    Initial Setup

---
+++ easter_date.py
@@ -0,0 +1,20 @@
+# File: easter_date.py
+# Description: <A DESCRIPTION OF YOUR PROGRAM>
+# Assignment Number: 2
+#
+# Name: <YOUR NAME>
+# SID:  <YOUR EID>
+# Email: <YOUR EMAIL>
+# Grader: <YOUR GRADER'S NAME Carolyn OR Emma OR Ahmad>
+# Slip days used this assignment: <#>
+#
+# On my honor, <YOUR NAME>, this programming assignment is my own work
+# and I have not provided this code to any other student. .
+
+
+def main():
+    pass # Replace this line with all of your code
+
+
+if __name__ == "__main__":
+    main()
---
+++ easter_date_test.py
@@ -0,0 +1,22 @@
+#Do not modify the code in this file
+#Test for easter date
+
+import os.path
+import sys
+from easter_date import main
+from tud_test_base import *
+
+def easter_date_test():
+    try:
+        exists = os.path.exists("easter_date.py")
+        assert  exists == True
+    except:
+        sys.exit()
+
+    set_keyboard_input([2001])
+    main()
+    output = get_display_output()
+    assert output == [
+        "Enter year: ",
+        "In 2001 Easter Sunday is on 4/15/2001."
+    ]
